51b839641196da7ec9a5d1015a4c3e4ea26386bb
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-12 15:42:53 +0200

    Add control lines for blueprint and ModelExport name to ExportController

--- app/Http/Controllers/API/ExportController.php
+++ app/Http/Controllers/API/ExportController.php
@@ -12,6 +12,7 @@ class ExportController extends Controller
     public function exportExcel() 
     {
         if (!request()->has('blueprint')) abort(403, 'A blueprint is needed');
+        if (!request('blueprint')) abort(403, 'A blueprint is needed');
         if (strpos(request('model'), '_')) {
             $array = explode('_', request('model'));
             $array[count($array) - 1] = Str::singular($array[count($array) - 1]);
@@ -27,6 +28,7 @@ class ExportController extends Controller
             return Excel::download(new \App\Exports\WildCardViewExports($modelClass), $name . '.xlsx');
         }
         $exportClass = '\App\Exports\\' . $name . 'Exports';
+        if (!class_exists($exportClass)) abort(403, 'This Model is not exportable.');
         if (!in_array(request('blueprint'), (new $exportClass)::$blueprints)) abort(403, 'Blueprint doesn\'t exists.');
         return Excel::download(new $exportClass, $name . '.xlsx');
     }
