54c984bc4a754059cc0b2c87ed5392212cdb8738
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-12 15:58:44 +0200

    Redone authorize method in Qmodel. Grant access always to root and load user relationships on demand

--- app/Qmodel.php
+++ app/Qmodel.php
@@ -58,25 +58,6 @@ class Qmodel extends Model
     ];
     // END Quasar DATA
 
-
-    // public function getShowView($view=null) {
-    //   $views = [
-    //     'distributions' => [
-    //       'county',
-    //       'cost_center',
-    //       'addresses',
-    //       'phones',
-    //       'area_manager',
-    //       'clinic_manager',
-    //       'clinic_poster_priorities',
-    //       'poster_distributions' => function ($q) { return $q->with(['original_facade', 'complete_facades']); }
-    //     ],
-    //     'default' => static::$show ? static::$show : []
-    //   ];
-    //   if (!$view) $view = 'default';
-    //   else if (!array_key_exists($view, $views)) $view = 'default';
-    //   return $views[$view];
-    // }
     public function getShowView($view=null) {
       if (!$view) {
         $view = static::$show ? static::$show : [];
@@ -86,43 +67,21 @@ class Qmodel extends Model
       return static::$views[$view];
     }
     protected static function authorize($method) {
-      // dump('Authorizing');
-      // dump(static::class);
-      // return false;
-      // if (static::class === 'App\Group') return true;
-      // if (static::class === 'App\GroupUser') return true;
-      if(!array_key_exists($method, static::$permissions)) return false;
-      if (in_array('*', static::$permissions[$method])) return true;
       $user = auth()->guard('api')->user();
       if ($user->isRoot()) return true;
+
+      if(!array_key_exists($method, static::$permissions)) return false;
+      if (in_array('*', static::$permissions[$method])) return true;
+
+      $user->append('groupsInfo');
       $userGroups = array_keys($user->groupsInfo);
-      // if (count($this->permissions)) {
-      //   foreach ($userGroups as $group) {
-      //     if ($this->permissions[$group]) {
-      //       $role = $user->groupsInfo[$group];
-      //       if (arra_key_exists($role, $this->permissions[$group])) {
-      //         if (in_array($this->permissions[$group][$role], $method)) return true;
-      //       }
-      //     }
-      //   }
-      // }
-      // dump($userGroups);
-      // dump($method);
-      // dump(static::$permissions);
       foreach ($userGroups as $group) {
-        // dump('Here');
         if (array_key_exists($group, static::$permissions[$method])) {
           if (in_array('*', static::$permissions[$method][$group])) return true;
           $role = $user->groupsInfo[$group];
-          // dump('THere');
-          // dump($role);
-          // dump(in_array($role, static::$permissions[$method][$group]));
           if (in_array($role, static::$permissions[$method][$group])) return true;
         }
       }
-      // return response([
-      //   'message' => 'Unauthorized action.',
-      // ], 403);
       abort(403, 'Unauthorized action.');
     }
 }
