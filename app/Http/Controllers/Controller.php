5051318f189caa9e9c8f7396a552a6491e49c58a
JG Villalba <jgvillalba@mozodealmacen.com>
2019-09-19 09:32:15 +0200

    New methods to build model and form request names base on static class. New store method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,20 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QStore;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public $requestName, $requestBaseName;
+
+    public function __construct()
+    {
+        $this->requestBaseName = $this->getbaseRequestModelName();
+        $this->requestName = $this->getRequestModelName();
+    }
+
+    protected function getBaseModelName()
+    {
+        $baseName = substr(static::class, strlen('App\Http\Controllers\API\\'));
+        $baseName = substr($baseName, 0, strlen($baseName) - strlen('Controller'));
+        return $baseName;
+    }
+
     protected function getModelName()
     {
-        $modelName = substr(static::class, strlen('App\Http\Controllers\API\\'));
-        $modelName = '\App\\' . substr($modelName, 0, strlen($modelName) - strlen('Controller'));
-        return $modelName;
+        return '\App\\' . $this->getBaseModelName();
+    }
+
+    protected function getBaseRequestModelName()
+    {
+        return 'Store' . $this->getBaseModelName();
+    }
+
+    protected function getRequestModelName()
+    {
+        return '\App\Http\Requests\Store' . $this->getBaseModelName();
     }
 
     /**
@@ -25,11 +50,10 @@ class Controller extends BaseController
      */
     public function index()
     {
-        $modelName = substr(static::class, strlen('App\Http\Controllers\API\\'));
-        $modelName = '\App\\' . substr($modelName, 0, strlen($modelName) - strlen('Controller'));
+        // dd($this->requestName);
         return response([
-            'model' => $modelName::fetch(),
-            'quasarData' => $modelName::getQuasarData(),
+            'model' => $this->getModelName()::fetch(),
+            'quasarData' => $this->getModelName()::getQuasarData(),
         ], 200);
     }
 
@@ -45,7 +69,27 @@ class Controller extends BaseController
         $model->getView(request()->has('view') ? request('view') : null);
         return response([
             'model' => $model
-            // 'quasarData' => $this->getModelName()::getQuasarData(),
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(QStore $request)
+    {
+        // $requestName = $this->getRequestModelName();
+        // $rules = (new $requestName)->rules();
+        // $validated = request()->validate($rules);
+
+        // $id = $this->getModelName()::create($validated)->id;
+        $id = $this->getModelName()::create(request()->all())->id;
+        $model = $this->getModelName()::fetch(null, null, null, false, [$id])[0];
+
+        return response([
+            'model' => $model,
         ], 200);
     }
     
@@ -56,15 +100,13 @@ class Controller extends BaseController
      * @return \Illuminate\Http\Response
      */
     public function destroy($id) {
-        // dd(request()->all());
-        $modelName = substr(static::class, strlen('App\Http\Controllers\API\\'));
-        $modelName = '\App\\' . substr($modelName, 0, strlen($modelName) - strlen('Controller'));
+        $name = $this->getModelName();
 
-        $model = $modelName::fetch(null, null, null, false, [$id])[0];
-        if (isset($this->getModelName()::$cascade)) {
-            foreach ($this->getModelName()::$cascade as $relation) $model->$relation()->delete();
+        $model = $name::fetch(null, null, null, false, [$id])[0];
+        if (isset($name::$cascade)) {
+            foreach ($name::$cascade as $relation) $model->$relation()->delete();
         }
-        $modelName::destroy($id);
+        $name::destroy($id);
 
         return response([
             'model' => array_key_exists('deleted_at', $model->toArray()) ? $model->fresh() : $model,
