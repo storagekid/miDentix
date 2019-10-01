dd0f63d77f3e7fed9b4fe2ef8b5e6e52f9944b62
JG Villalba <jgvillalba@mozodealmacen.com>
2019-10-01 11:46:16 +0200

    Use active_distributions from Clinic Model. Remove comments

--- app/Printers/CampaignFacadeDistributionPrinter.php
+++ app/Printers/CampaignFacadeDistributionPrinter.php
@@ -3,11 +3,8 @@
 namespace App\Printers;
 
 use App\Printers\DentixPdfPrinter;
-// use TCPDF;
-use setasign\Fpdi\Tcpdf\Fpdi;
 use App\Clinic;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Storage;
 
 class CampaignFacadeDistributionPrinter extends DentixPdfPrinter
 {
@@ -27,35 +24,21 @@ class CampaignFacadeDistributionPrinter extends DentixPdfPrinter
         $this->hasFoam = $this->clinic->clinic_posters()->first()->poster->material === 'FOAM' ? true : false;
         $campaignDate = \Carbon\Carbon::parse($this->campaign->starts_at);
         $this->dateString = ucfirst($campaignDate->locale('es')->monthName) . ' ' . $campaignDate->year;
-        // $dists = $clinic->poster_distributions->filter(function($i) { return $i->campaign_id === $this->campaign->id; });
-        // if (!count($dists)) $dists = $clinic->poster_distributions->filter(function($i) { return $i->campaign_id === null; });
-        $clinicDists = $clinic->clinic_distributions_by_campaign;
-        array_key_exists($this->campaign->id, $clinicDists) ? $dists = $clinicDists[$this->campaign->id] : $dists = $clinicDists[''];
-        // $dists = $clinicDists[$this->campaign->id] || $clinicDists[''];
-        // if (!count($dists)) $dists = $clinicDists[''];
-        // dd($dists);
-        foreach ($dists as $dist) {
+
+        $clinicDists = $clinic->active_distributions($this->campaign);
+
+        foreach ($clinicDists as $dist) {
           if (!$dist->composed_facade) {
-            // dump('No Composed');
             $dist->composeFacadeBuilder();
-            // dd($dist->composed_facade()->first());
           }
           $completeFacade = $dist->complete_facades()->where('campaign_id', request('campaign'))->first();
           if (!$completeFacade || request('force') == 'true') {
             $completeFacade = $dist->completeFacadeBuilder();
           }
-          // if (!$dist->complete_facade) {
-          //   // dump('No Complete');
-          //   $dist->completeFacadeBuilder();
-          //   // dd($dist->complete_facade()->first());
-          // }
-          // $file = $dist->complete_facade()->first();
-          // if (!$file) dd('No Completed');
           $this->files[] = $completeFacade->complete_facade()->first();
         }
         $this->fileName = $this->clinic->cleanName . '-' . $this->campaign->name . '-posters.pdf';
         $this->pathToFile = storage_path('app/' . $this->directory . '/' . $this->fileName);
-        // dd($this->files);
     }
 
     public function makePdf()
@@ -102,11 +85,6 @@ class CampaignFacadeDistributionPrinter extends DentixPdfPrinter
 
       $this->pdf->setY(10, true);
       $this->pdf->SetFont($this->fontDxLight,'',18,'',false);
-      // $text = "Instrucciones";
-      // $this->pdf->MultiCell('','',$text,0,'L',0,1,$marginLeft,'',false,0,false,true,0,'T',false);
-      // $this->pdf->Image($this->foamImage);
-      // dump($this->hasFoam);
-      // $this->pdf->ImageEps($this->logoWhite, $marginRight, $this->pdf->getY(), '', $logoHeight, '', true, 'L', '', 0, false);
       $html = (string)View::make('exports.distributionGuide', ['hasFoam' => $this->hasFoam, 'clinic' => $this->clinic, 'image' => $this->hasFoam ? $this->foamImage : $this->translightImage]);
       $this->pdf->setX(10);
       $this->pdf->writeHTML($html, true, false, true, false, '');
@@ -118,12 +96,6 @@ class CampaignFacadeDistributionPrinter extends DentixPdfPrinter
 
       $logoHeight = 20;
       $this->pdf->ImageEps($this->logoClaim512,0, ($size[1]-$logoHeight) / 2, '', $logoHeight, '', true, 'M', 'C', 0, false);
-
-      // $this->fileName = $this->clinic->cleanName . '-' . $this->campaign->name . '-posters.pdf';
-      // if (!Storage::exists('public/clinics/')) {
-      //   Storage::makeDirectory('public/clinics/');
-      // }
-      // $this->pathToFile = storage_path('app/public/clinics/' . $this->fileName);
 
       $this->pdf->Output($this->pathToFile, 'F');
       return $this;
