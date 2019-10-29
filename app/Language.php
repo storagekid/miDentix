dc9d110233f26e579ce576c14bbc9ab80eef6fdf
JG Villalba <jgvillalba@mozodealmacen.com>
2019-10-29 10:04:06 +0100

    Add Table and Filling Info

--- app/Language.php
+++ app/Language.php
@@ -4,8 +4,10 @@ namespace App;
 
 class Language extends Qmodel
 {
+    protected $fillable = ['iso_name','native_name', '639-1', '639-2/T', '639-2/B', '639-3', 'notes'];
+    protected static $full = [];
     protected static $permissions = [
-        'view' => ['*']
+      'view' => ['*']
     ];
 
     // Quasar DATA
@@ -59,4 +61,30 @@ class Language extends Qmodel
     ];
     protected $keyField = 'native_name';
     // END Quasar DATA
+
+    // Tableable DATA
+    protected $tableColumns = [
+        'iso_name' => [
+            'label' =>'ISO Name',
+        ],
+        'native_name' => [
+            'label' =>'Native Name',
+        ],
+        '639-1' => [
+            'label' =>'639-1 Code',
+        ],
+        '639-2/T' => [
+            'label' =>'639-2/T Code',
+        ],
+        '639-2/B' => [
+            'label' =>'639-2/B Code',
+        ],
+        '639-3' => [
+            'label' =>'639-3 Code',
+        ],
+        'notes' => [
+            'label' =>'Notes',
+        ]
+    ];
+    // END Table Data
 }
