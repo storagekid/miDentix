c73edfa276b0c37a4218f3669fc4112158477d48
JG Villalba <jgvillalba@mozodealmacen.com>
2019-10-29 10:07:44 +0100

    Add routes for Scope and Mailing

--- routes/api.php
+++ routes/api.php
@@ -33,23 +33,32 @@ Route::prefix('rest')->namespace('api')->group(function() {
 
         Route::resource('profiles', 'ProfileController');
         Route::resource('clinics', 'ClinicController');
+        Route::post('clinics/{clinic}/clinic_siblings', 'ClinicSiblingController@store');
         Route::post('clinics/{clinic}/posterDistribution', 'ClinicController@posterDistributionComposer');
         Route::post('clinics/{clinic}/posterDistributionByCampaing', 'ClinicController@posterDistributionByCampaignComposer');
         Route::post('clinics/{clinic}/posterDistributionClone', 'ClinicController@posterDistributionClone');
         Route::post('clinics/{clinic}/posterDistributionLauncher', 'ClinicController@launchCampaignDistribution');
+        Route::resource('clinic_siblings', 'ClinicSiblingController');
 
 
+        Route::resource('stands', 'StandController');
         Route::resource('cost_centers', 'CostCenterController');
         Route::resource('jobs', 'JobController');
 		Route::resource('job_types', 'JobTypeController');
         Route::resource('companies', 'CompanyController');
         Route::resource('countries', 'CountryController');
+        Route::resource('states', 'StateController');
+        Route::resource('counties', 'CountyController');
         Route::resource('stores', 'StoreController');
         Route::resource('emails', 'EmailController');
         Route::resource('phones', 'PhoneController');
         Route::resource('addresses', 'AddressController');
         Route::resource('sanitary_codes', 'SanitaryCodeController');
-        
+        Route::resource('legals', 'LegalController');
+        Route::resource('files', 'FileController');
+        Route::resource('languages', 'LanguageController');
+        Route::resource('currencies', 'CurrencyController');
+
         Route::resource('clinic_schedules', 'ClinicScheduleController');
         Route::post('/clinic_schedules/download-tags', 'ClinicScheduleController@downloadTags');
 
@@ -72,10 +81,30 @@ Route::prefix('rest')->namespace('api')->group(function() {
         Route::get('poster_distributions/{clinicposterdistribution}/complete', 'ClinicPosterDistributionController@completeFacade');
         Route::get('poster_distributions/{clinicposterdistribution}/compose', 'ClinicPosterDistributionController@composeFacade');
         Route::get('poster_distributions/{clinicposterdistribution}/removeFacade', 'ClinicPosterDistributionController@removeFacade');
-        Route::resource('languages', 'LanguageController');
         Route::resource('posters', 'PosterController');
         Route::resource('poster_models', 'PosterModelController');
-        Route::resource('files', 'FileController');
+
+        Route::resource('mailings', 'MailingController');
+        Route::resource('mailings/{mailing}/mailing_designs', 'MailingDesignController');
+        Route::post('mailings/{mailing}/mailinigDataUploader', 'MailingController@mailinigDataUploader');
+        Route::resource('mailing_designs', 'MailingDesignController');
+        // Route::resource('mailing_designs/{mailing_design}/clinic_mailing_designs', 'ClinicMailingDesignController');
+        Route::post('mailing_designs/{mailing_design}/clinicMailingGenerator', 'MailingDesignController@clinicMailingGenerator');
+        Route::post('mailing_designs/{mailing_design}/ProductAndServicesGenerator', 'MailingDesignController@productAndServicesGenerator');
+        Route::post('mailing_designs/{mailing_design}/IndesignCSVGenerator', 'MailingDesignController@indesignCSVGenerator');
+        Route::post('mailing_designs/{mailing_design}/IndesignRenameGenerator', 'MailingDesignController@indesignRenameGenerator');
+        Route::resource('clinic_mailings', 'ClinicMailingController');
+        Route::patch('clinic_mailings/{clinic_mailing}/saveAF', 'ClinicMailingController@saveAF');
+        Route::resource('promotions', 'PromotionController');
+        Route::resource('claims', 'ClaimController');
+
+        Route::resource('providers', 'ProviderController');
+        Route::resource('products', 'ProductController');
+        Route::resource('services', 'ServiceController');
+        Route::resource('product_providers', 'ProductProviderController');
+        Route::resource('service_providers', 'ServiceProviderController');
+        Route::post('services/{service}/service_providers', 'ServiceProviderController@store');
+        Route::post('products/{product}/product_providers', 'ProductProviderController@store');
 
         Route::post('users/{user}/group_users', 'GroupUserController@store');
         Route::post('profiles/{profile}/clinic_profiles', 'ClinicProfileController@store');
