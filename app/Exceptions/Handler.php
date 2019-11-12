92114dd80cad4b2289b0bf482ab35f7ad1c3168d
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-12 13:40:58 +0100

    New Exception for PosterDistribution Errors. Add that Exception to dontReport Array in Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -19,7 +19,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        PosterDistributionException::class
+        PosterDistributionException::classAdd
     ];
 
     /**
