d263b20ec690ee18f3dc21da04497459b354792f
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-18 10:10:57 +0100

    Revamp __costruct to work with poster_starts_at instead of campaign Start Date

--- app/Mail/PosterDistributionNotification.php
+++ app/Mail/PosterDistributionNotification.php
@@ -22,37 +22,40 @@ class PosterDistributionNotification extends Mailable
         $this->pdf = $file;
         $this->name = $name;
         $campaignDate = \Carbon\Carbon::parse($campaign->starts_at);
-        $day = $campaignDate->dayOfWeekIso;
-        if ($day === 7) {
-            $day = 1;
-            $campaignDate->addDay();
-        }
-        $dayString = '';
-        switch ($day) {
-            case 1:
-                $dayString = 'lunes';
-                break;
-            case 2:
-                $dayString = 'martes';
-                break;
-            case 3:
-                $dayString = 'miércoles';
-                break;
-            case 4:
-                $dayString = 'jueves';
-                break;
-            case 5:
-                $dayString = 'viernes';
-                break;
-            case 6:
-                $dayString = 'sábado';
-                break;
-            default:
-                $dayString = 'domingo';
-                break;
+        if ($campaign->poster_starts_at) {
+            $postersDate = \Carbon\Carbon::parse($campaign->poster_starts_at);
+            $day = $postersDate->dayOfWeekIso;
+            if ($day === 7) {
+                $day = 1;
+                $postersDate->addDay();
+            }
+            $dayString = '';
+            switch ($day) {
+                case 1:
+                    $dayString = 'lunes';
+                    break;
+                case 2:
+                    $dayString = 'martes';
+                    break;
+                case 3:
+                    $dayString = 'miércoles';
+                    break;
+                case 4:
+                    $dayString = 'jueves';
+                    break;
+                case 5:
+                    $dayString = 'viernes';
+                    break;
+                case 6:
+                    $dayString = 'sábado';
+                    break;
+                default:
+                    $dayString = 'domingo';
+                    break;
+            }
+            $this->postersDate = $dayString . ' ' . $postersDate->day . ' de ' . $postersDate->locale('es')->monthName;
         }
         $this->subject = 'Distribución Cartelería Campaña ' . ucfirst($campaignDate->locale('es')->monthName) . ' ' . $campaignDate->year;
-        $this->postersDate = $dayString . ' ' . $campaignDate->day . ' de ' . $campaignDate->locale('es')->monthName;
     }
 
     /**
@@ -65,7 +68,7 @@ class PosterDistributionNotification extends Mailable
         // dump('MAIL REACHED');
         return $this->from(['address' => 'marketing@dentix.es', 'name' => 'Dpto. Marketing Dentix'])
             ->markdown('emails.posterDistributionNotification')
-            ->subject('Distribución Cartelería Campaña Septiembre 2019')
+            ->subject($this->subject)
             ->attach($this->pdf, [
                 'as' => $this->name
                 ]);
