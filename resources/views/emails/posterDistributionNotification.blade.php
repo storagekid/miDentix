34fd557720d6e227f86123a9091cd122a9b92fa9
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-18 09:44:11 +0100

    Rewrite email notification for poster distributions

--- resources/views/emails/posterDistributionNotification.blade.php
+++ resources/views/emails/posterDistributionNotification.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-# Buenos días,
+# Hola a todos.
  
-Adjunto os enviamos un documento especialmente diseñado para vuestra clínica sobre la cartelería de la nueva campaña, que deberéis colocar el próximo {{ $postersDate }}.
+Adjunto os enviamos un documento especialmente diseñado para vuestra clínica sobre la cartelería de la nueva campaña, que deberéis colocar @if($postersDate) el próximo {{ $postersDate }}. @else tan pronto como la recibáis. @endif<br>
 
-Por favor, seguid las indicaciones de este documento y si tenéis cualquier duda, escribid un email a  <a href="mailto:marketing@dentix.es">marketing@dentix.es</a>
+Por favor, seguid las indicaciones de este documento y si tenéis cualquier duda, escribid un email a  <a href="mailto:marketing@dentix.es">marketing@dentix.es</a>.
 
 Atentamente.<br>
 El equipo de Marketing.
