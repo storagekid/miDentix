e2b2687d4f5f7a89d91db0013b8f0f72c7555c68
JG Villalba <jgvillalba@mozodealmacen.com>
2019-11-20 11:52:15 +0100

    Add Support for new priorities 9,10,11,12

--- app/ClinicPosterPriority.php
+++ app/ClinicPosterPriority.php
@@ -60,7 +60,7 @@ class ClinicPosterPriority extends Qmodel
             'label' =>'Priority',
             'type' => [
                 'name' =>'array',
-                'array' => [1,2,3,4,5,6,7,8],
+                'array' => [1,2,3,4,5,6,7,8,9,10,11,12],
                 'default' => [
                     'text' => 'Selecciona una Prioridad',
                 ],
