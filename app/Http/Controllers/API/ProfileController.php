44a08994645647f00940dfb04cb6b4362b4fedd0
JG Villalba <jgvillalba@mozodealmacen.com>
2019-12-18 11:17:04 +0100

    Move update method to Base Controller

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -8,21 +8,4 @@ use App\Profile;
 
 class ProfileController extends Controller
 {
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(QStore $request, $id)
-    {
-        // dd(request()->all());
-        $model = Profile::find($id);
-        $model->update(request()->all());
-
-        return response([
-            'model' => $model->attachFull(),
-        ], 200);
-    }
 }
