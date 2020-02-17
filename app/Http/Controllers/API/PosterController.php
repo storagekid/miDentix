f454d71c4d47fe2f665d62e042f40853c12cca43
JG Villalba <jgvillalba@mozodealmacen.com>
2020-02-17 10:48:28 +0100

    Remove unused Imports

--- app/Http/Controllers/API/PosterController.php
+++ app/Http/Controllers/API/PosterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\QStore;
 
 class PosterController extends Controller
 {
--- app/Http/Controllers/API/PosterModelController.php
+++ app/Http/Controllers/API/PosterModelController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\QStore;
-use App\PosterModel;
 
 class PosterModelController extends Controller
 {
